aee8312866467d9b2b811f25b0e4afe4ec42429c
Ton <146097352+wiangin@users.noreply.github.com>
2025-04-18 10:49:15 +0200

    wip

--- server/index.js
+++ server/index.js
@@ -1,2 +1,29 @@
-const hej = require('hej')
-const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Pusher = require('pusher');
+const express = require('express');
+
+require('dotenv').config();
+require('dotenv').config({ 
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
+const app = express();
+const port = 3001;
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  useTLS: true,
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
